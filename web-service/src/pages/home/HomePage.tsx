b1d102489a5dd192a366c3119d6fb86d93c45f7b
Seokho <happt7889@gmail.com>
2025-07-02 11:18:35 +0900

    [add] 기본 Homepage Component

---
+++ web-service/src/pages/home/HomePage.tsx
@@ -0,0 +1,17 @@
+import GreetInput from "@/features/greet-user/ui/GreetInput";
+import UserGreeting from "@/entities/user/ui/UserGreeting";
+import { useGreet } from "@/features/greet-user/model/useGreet";
+
+function HomePage() {
+  const { name, setName } = useGreet();
+
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Home Page</h1>
+      <GreetInput name={name} onChange={setName} />
+      <UserGreeting name={name} />
+    </div>
+  );
+}
+
+export default HomePage;
