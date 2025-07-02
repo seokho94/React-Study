1fc3711f8a67d603d2a184ff6f25542accb0936a
Seokho <happt7889@gmail.com>
2025-07-02 11:17:24 +0900

    [add] Props 값으로 텍스트 표출 component

---
+++ web-service/src/entities/user/ui/UserGreeting.tsx
@@ -0,0 +1,10 @@
+type Props = {
+    name: string;
+}
+
+function UserGreeting({ name }: Props) {
+    if(!name) return null;
+    return <p style={{ marginTop: '1rem'}}>안녕하세요, {name}님!</p>;
+}
+
+export default UserGreeting;
