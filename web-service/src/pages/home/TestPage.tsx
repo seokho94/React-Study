e992f8ffc0feaa7cbbc724ce455bbc8b0af813ab
Seokho <happt7889@gmail.com>
2025-07-03 10:49:23 +0900

    [feat] Page Path 및 명칭 변경

--- web-service/src/pages/home/TestPage.tsx
+++ web-service/src/pages/home/TestPage.tsx
@@ -2,16 +2,16 @@ import GreetInput from "@/features/greet-user/ui/GreetInput";
 import UserGreeting from "@/entities/user/ui/UserGreeting";
 import { useGreet } from "@/features/greet-user/model/useGreet";
 
-function HomePage() {
+function TestPage() {
 	const { name, setName } = useGreet();
 
 	return (
 		<div style={{ padding: '2rem' }}>
-			<h1>Home Page</h1>
+			<h1>Test Page</h1>
 			<GreetInput name={name} onChange={setName} />
 			<UserGreeting name={name} />
 		</div>
 	);
 }
 
-export default HomePage;
+export default TestPage;
