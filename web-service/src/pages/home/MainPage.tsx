5c889f15074404ddafa8fe1f31776ab4173c6085
Seokho <happt7889@gmail.com>
2025-07-03 10:29:51 +0900

    [add] 기본 레이아웃 설정 추가

---
+++ web-service/src/pages/home/MainPage.tsx
@@ -0,0 +1,12 @@
+import BaseLayout from "@/shared/ui/BaseLayout";
+
+function MainPage() {
+	return (
+		<BaseLayout>
+			<h1>Main Page</h1>
+			<p>Welcome to the main page!</p>
+		</BaseLayout>
+	);
+}
+
+export default MainPage;
